0cbaa65253f875fc561b847bc6fdc28a757d3cb1
shoutingminds-GY <shoutingminds@gmail.com>
2026-03-31 01:09:04 +0530

    Add files via upload

---
+++ api/ticker.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  try {
+    const response = await fetch('https://api.coindcx.com/exchange/ticker');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
